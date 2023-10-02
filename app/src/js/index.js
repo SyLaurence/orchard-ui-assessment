d5da02bcd78aef1668de339d4a6344fd2f29db64
Laurence Ivan Sy <sylaurence1997@gmail.com>
2023-10-03 00:25:10 +0800

    refactor: added mobile responsiveness (320px) to All the lates setion

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -5,23 +5,30 @@ const modalDiv = document.getElementById('modal')
 // Modal img element
 const modalImg = document.getElementById('modal-img')
 
+// Body element
+const body = document.querySelector('body')
+
 
 /**
  * Reusable Image modal
  * Shows the image modal by removing the 'hidden' class
+ * Disables scroll on body
  * 
  * @param {string} src 
  */
 function showModal(src) {
     modalImg.src = src;
+    body.classList.add('overflow-hidden')
     modalDiv.classList.remove('hidden')
 }
 
 /**
  * Hides the image modal by adding a 'hidden' classs
+ * Enables scroll on body
  * 
  */
 function hideModal() {
+    body.classList.remove('overflow-hidden')
     modalDiv.classList.add('hidden')
 }
 
